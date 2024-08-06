af9742a8488ecf736cdcebd54297ac7f1b07dd29
tharsihav <tharsihav@gmail.com>
2024-08-06 14:19:43 +1000

    fixing navabr hover issue kinda

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
-import { Link } from 'wouter';
 import './Navbar.css';
+import { Link } from 'wouter';
 
-// import * as React from 'react';
+import * as React from 'react';
 import Menu from '@mui/joy/Menu';
 import MenuItem from '@mui/joy/MenuItem';
 import IconButton from '@mui/joy/IconButton';
@@ -10,41 +10,51 @@ import Person from '@mui/icons-material/Person';
 import Home from '@mui/icons-material/Home';
 
 const Navbar = () => {
+  const [notificationOpen, setNotificationOpen] = React.useState(false);
+  const [profileOpen, setProfileOpen] = React.useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link href="/" className="navbar-logo">TravelSite(logo)</Link>
+        <Link href="/" className="navbar-item">TravelSite(logo)</Link>
         <ul className="navbar-menu">
-          <li>
-            <Link href="/home" className="navbar-item">
+          <li className="navbar-item">
+            <Link href="/home">
               <IconButton>
                 <Home className="navbar-icon" />
               </IconButton>
             </Link>
           </li>
-          <li>
-            <Link href="/friends" className="navbar-item">
+          <li className="navbar-item">
+            <Link href="/friends">
               <IconButton>
                 <Person className="navbar-icon" />
               </IconButton>
             </Link>
           </li>
           <li className="navbar-item">
-            <IconButton>
+            <IconButton
+              onMouseEnter={() => setNotificationOpen(true)}
+              onMouseLeave={() => setNotificationOpen(false)}
+            >
               <Notifications className="navbar-icon" />
             </IconButton>
-            <div className="navbar-dropdown-menu">
-              <Menu>
+            <div className='navbar-dropdown-menu'>
+              <Menu open={notificationOpen}>
                 <MenuItem>No notifications yet</MenuItem>
               </Menu>
             </div>
           </li>
           <li className="navbar-item">
-            <IconButton>
-              <Person className="navbar-icon"/>
+            <IconButton
+              onMouseEnter={() => setProfileOpen(true)}
+              onMouseLeave={() => setProfileOpen(false)}
+            >
+              <Person className="navbar-icon" />
             </IconButton>
-            <div className="navbar-dropdown-menu">
-              <Menu>
+            <div className='navbar-dropdown-menu'>
+              <Menu open={profileOpen}>
+                <MenuItem>Edit Profile</MenuItem>
                 <MenuItem>Settings</MenuItem>
                 <MenuItem>Logout</MenuItem>
               </Menu>
