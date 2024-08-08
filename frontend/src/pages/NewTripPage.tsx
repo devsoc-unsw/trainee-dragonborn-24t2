849b13c0917366bd4087fabf9d99c7a178c9f544
Sylvia Wang <sylviamiaowang@gmail.com>
2024-08-08 14:21:05 +1000

    state stuff

--- frontend/src/pages/NewTripPage.tsx
+++ frontend/src/pages/NewTripPage.tsx
@@ -1,7 +1,18 @@
 import { Button, Input, Stack, Typography } from '@mui/joy';
+import React, { useState } from 'react';
 
 
 const NewTripPage = () => {
+  const [location, setLocation] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLocation(event.target.value)
+  }
+
+  const handleClick = () => {
+    console.log("we are travelling to " + location);
+  }
+
   return (
     <Stack height="100%" justifyContent="center" alignItems="center">
       <Typography level="h1">New Trip</Typography>
@@ -19,7 +30,7 @@ const NewTripPage = () => {
           <Typography level="body-lg" fontWeight="bold">Location</Typography>
           <Input sx={{
             width: "100%",
-          }}/>
+          }} onChange={handleInputChange}/>
         </Stack>
         <Stack direction="row" justifyContent="space-between" width="50%">
           <Stack gap={1}>
@@ -38,7 +49,12 @@ const NewTripPage = () => {
             sx={{ width: "100%", alignItems: "center" }}
           />
         </Stack>
-        <Button sx={{ width: "50%" }} color="warning" variant="solid">Plan Trip!</Button>
+        <Button 
+        sx={{ width: "50%" }} 
+        color="warning" 
+        variant="solid"
+        onClick={handleClick}
+        >Plan Trip!</Button>
       </Stack>
     </Stack>
   );
