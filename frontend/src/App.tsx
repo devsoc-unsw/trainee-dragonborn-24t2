726817e03ffd750537d2e659f6a5f0bacf08be40
Franco Reyes <79299724+francojreyes@users.noreply.github.com>
2024-09-04 16:22:35 +1000

    Use reactfire and make helper funcs (#2)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,39 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Route, Switch } from 'wouter';
-import LoginPage from './pages/LoginPage.tsx';
-import HomePage from './pages/HomePage.tsx';
-import { Button, CssBaseline, CssVarsProvider } from '@mui/joy';
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button, CssBaseline, CssVarsProvider } from "@mui/joy";
+import { getFirestore } from "firebase/firestore";
+import { useState } from "react";
+import { FirestoreProvider, useFirebaseApp, } from "reactfire";
+import { Route, Switch } from "wouter";
+import reactLogo from "./assets/react.svg";
+import HomePage from "./pages/HomePage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
 
 function App() {
-
+  const firestoreInstance = getFirestore(useFirebaseApp());
   return (
     <CssVarsProvider>
-      <CssBaseline />
-      <Switch>
-        <Route path="/" component={LandingPage} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/home" component={HomePage} />
-      </Switch>
+      <CssBaseline/>
+      <FirestoreProvider sdk={firestoreInstance}>
+        <Switch>
+          <Route path="/" component={LandingPage}/>
+          <Route path="/login" component={LoginPage}/>
+          <Route path="/home" component={HomePage}/>
+        </Switch>
+      </FirestoreProvider>
     </CssVarsProvider>
-
-  )
+  );
 }
 
 const LandingPage = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <Button variant="solid">Hello world</Button>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo"/>
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt="React logo"/>
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -49,7 +52,7 @@ const LandingPage = () => {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
