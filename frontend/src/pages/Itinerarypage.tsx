6e68b18ae27fed3c1b3a3f409fd75a36a2f2928a
Bea Dela Cruz <z5477991@ad.unsw.edu.au>
2024-09-12 09:59:30 +1000

    styled the day component

--- frontend/src/pages/Itinerarypage.tsx
+++ frontend/src/pages/Itinerarypage.tsx
@@ -6,29 +6,65 @@ import {
 
 import  WbSunnyIcon from '@mui/icons-material/WbSunny';
 
+const eventStartTime: number = 20;
+const eventEndTime: number = 23.5;
+const dayLength: number = 24-7;
+const dayStart: number = 7;
+
+const timeblockHeight = (startTime: number, endTime: number) => {
+  return (endTime - startTime) / dayLength;
+}
+
+const topRelative = (startTime: number ) => {
+  const res = ((startTime - dayStart) / dayLength) * 100;
+  console.log(res)
+  return res;
+}
+
+const date = '4/09/2024';
+const day = 'Monday'; 
+const eventName = 'hiking';
+
+const shadow = (theme: any) => ({
+      boxShadow: theme.shadow.md,
+      '--joy-shadowChannel': theme.vars.palette.primary.mainChannel,
+      '--joy-shadowRing': 'inset 0 -3px 0 rgba(0 0 0 / 0.24)'})
+
 const ItineraryPage = () => {
 
-  const startTime: number = 9;
-  const endTime: number = 13.5;
-  const dayLength: number = 24-7;
 
-  const timeblockHeight = (startTime, endTime) => {
-    return (endTime - startTime) / dayLength;
-  }
   
     return (
-      <Stack justifyContent='center' alignItems='center' width='100%' height='100%'>
-        <Stack height='70%' width='20%' sx={{border:'solid', borderColor:'grey', borderRadius:'10px'}} flexDirection={"column"} p="20px" gap="10px">
-          <Stack bgcolor={"red"} flexDirection='row' justifyContent='space-between' alignItems='center' pt="30px">
+      <Stack justifyContent='center' alignItems='center' width='100%' height='100%' bgcolor='#fffafa'>
+        <Stack height='70%' width='20%' bgcolor='white' sx={{border:'solid', borderColor:'grey', borderRadius:'10px'}} flexDirection={"column"} p="20px" gap="10px">
+          <Stack flexDirection='row' justifyContent='space-between' alignItems='center' pt="30px">
             <Stack flexDirection='column'>
-              <Typography level="h3">Monday</Typography>
-              <Typography level="title-sm">4/09/2024</Typography>
+              <Typography level="h3">{day}</Typography>
+              <Typography level="title-sm">{date}</Typography>
             </Stack>
-            <WbSunnyIcon sx={{fontSize:'44px'}} />
+            <WbSunnyIcon sx={{fontSize:'44px', color:'var(--tertiary-color)'}} />
           </Stack>  
-          <Stack bgcolor="green" height="80%" >
-            <Stack bgcolor="blue" sx={{position:'relative', top:"40px"}} >hello</Stack>
-           
+          {/* navigation arrows */}
+
+          {/* one day schedule */}
+          <Stack bgcolor="f0f0f0" height="80%"  sx={
+							(theme) => ({
+								boxShadow: theme.shadow.md,
+								'--joy-shadowChannel': theme.vars.palette.primary.mainChannel,
+								'--joy-shadowRing': 'inset 0 -3px 0 rgba(0 0 0 / 0.24)', borderRadius:'10px'})}>
+            <Stack bgcolor='var(--tertiary-color)' color='white'
+            sx={
+							(theme) => ({
+								boxShadow: theme.shadow.md,
+								'--joy-shadowChannel': theme.vars.palette.primary.mainChannel,
+								'--joy-shadowRing': 'inset 0 -3px 0 rgba(0 0 0 / 0.24)', 
+                position:'relative', 
+                top:`${topRelative(eventStartTime)}%`, 
+                height:`${timeblockHeight(eventStartTime, eventEndTime)}`, 
+                borderRadius:'10px'
+              })}
+            justifyContent={'center'} alignItems='center'>{eventName}</Stack>
+
           </Stack>
         </Stack>
       </Stack>
